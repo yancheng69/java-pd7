8f21c86c63f280da04a68cb65398f150390511d7
yancheng69 <yanchengl3@nycstudents.net>
2026-09-18 16:28:35 +0000

    up

---
+++ Lesson-03b-Classwork/Main.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+
+  // Init function which runs when the 
+  // program begins.  
+
+  void init(){
+    System.out.println("Hello world");
+  }
+
+  
+}
